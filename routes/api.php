43b02fb855eb46b6c604aad30b1365ede11d1027
koffi kaikwe seglo <kofesto@hotmail.com>
2024-08-22 19:20:42 +0100

    jwt added

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', [UsersController::class,'login']);
+    Route::post('logout', [UsersController::class,'logout']);
+    Route::post('refresh', [UsersController::class,'refresh']);
+    Route::post('me', [UsersController::class,'me']);
 });
